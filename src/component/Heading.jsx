9149b89dee76d0cf9b74315a970546a2f1af7486
amankamboj2387 <amankamboj2387@gmail.com>
2021-02-04 16:47:43 +0530

    tutorial setup

---
+++ src/component/Heading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Heading(){
+return(
+    <h1>My name is Aman</h1>
+)
+
+}
+
+export default Heading;
---
+++ src/component/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List(){
+    return(
+        <ol>
+            <li>Aman</li>
+            <li>Kamboj</li>
+            <li>Kamboj institute</li>
+            <li>Plz work on it</li>
+        </ol>
+    );
+}
+
+export default List;
