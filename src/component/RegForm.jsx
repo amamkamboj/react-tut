96e3e540efa2b77dcdeffde5b7eb518b3a4244c2
amankamboj2387 <amankamboj2387@gmail.com>
2021-02-13 10:53:58 +0530

    Register form data with spread operator

--- src/component/RegForm.jsx
+++ src/component/RegForm.jsx
@@ -6,7 +6,8 @@ const RegForm = () => {
         fname: "",
         lname: "",
         email: "",
-        phone: ""
+        phone: "",
+        qual:  "",
 
     });
 
@@ -19,43 +20,53 @@ const RegForm = () => {
         // array destructuring
         const {value,name} = event.target;
         UpFormData((preVal)=>{
-            if(name === "fname"){
-                return{
-                    fname:value,
-                    lname:preVal.fname,
-                    email:preVal.email,
-                    phone:preVal.phone
-                };
+
+
+            return {
+                ...preVal,
+                [name]:value,
             }
-            else if(name === "lname"){
-                return{
-                    fname:preVal.fname,
-                    lname:value,
-                    email:preVal.email,
-                    phone:preVal.phone
-                };
-            }
-            else if(name === "email"){
-                return{
-                    fname:preVal.fname,
-                    lname:preVal.lname,
-                    email:value,
-                    phone:preVal.phone
-                };
-            }
-            else if(name === "phone"){
-                return{
-                    fname:preVal.fname,
-                    lname:preVal.lname,
-                    email:preVal.email,
-                    phone:value
-                };
-            }
+
+            // if(name === "fname"){
+            //     return{
+            //         fname:value,
+            //         lname:preVal.fname,
+            //         email:preVal.email,
+            //         phone:preVal.phone
+            //     };
+            // }
+            // else if(name === "lname"){
+            //     return{
+            //         fname:preVal.fname,
+            //         lname:value,
+            //         email:preVal.email,
+            //         phone:preVal.phone
+            //     };
+            // }
+            // else if(name === "email"){
+            //     return{
+            //         fname:preVal.fname,
+            //         lname:preVal.lname,
+            //         email:value,
+            //         phone:preVal.phone
+            //     };
+            // }
+            // else if(name === "phone"){
+            //     return{
+            //         fname:preVal.fname,
+            //         lname:preVal.lname,
+            //         email:preVal.email,
+            //         phone:value
+            //     };
+            // }
         })
     }
     return(
         <>
-        <h1>Login here {formData.fname}</h1>
+        <h1>Login here {formData.fname} {formData.lname}</h1>
+        <p> {formData.email} </p>
+        <p> {formData.phone} </p>
+        <p> {formData.qual} </p>
         <form>
             <input type="text" name="fname" onChange={InputChange} placeholder="Enter name" vlaue={formData.fname} />
             <br />
@@ -64,6 +75,8 @@ const RegForm = () => {
             <input type="email" name="email"  onChange={InputChange} vlaue={formData.email} placeholder="Enter email"/>
             <br />
             <input type="number" name="phone" onChange={InputChange} vlaue={formData.phone} placeholder="Enter phone number" />
+            <br />
+            <input type="text" name="qual" onChange={InputChange} vlaue={formData.qual} placeholder="Enter your qualification" />
             <br />
             <input type="submit" value="Regsiter" onSubmit={SubmitData} />
         </form>            
